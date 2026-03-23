2bf027d58367abe871c74b049a0734b4c40b41f5
Atharv <atharvgupta098@gmail.com>
2026-03-23 21:29:09 +0530

    Added loader to optimise & make experience faster in github

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
 import { data } from 'react-router-dom'
 import { useState } from 'react'
 
 function Github() {
+  const data = useLoaderData()
+    // const [data, setData] = useState([])
 
-    const [data, setData] = useState([])
-
-    useEffect(()=>{
-        fetch('https://api.github.com/users/AtharvGupta07')
-        .then(response => response.json())
-        .then(data => {
-            console.log('Gitdata:');
-            console.log(data);
-            setData(data)
-        })
-    },[])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/AtharvGupta07')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log('Gitdata:');
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // },[])
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl flex flex-col justify-between items-center'>
         <h1 className='p-4'>User Name: {data.login}</h1>
@@ -25,3 +26,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+  const response  = await fetch('https://api.github.com/users/AtharvGupta07')
+  return response.json()
+}
