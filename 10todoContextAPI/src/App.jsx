f206d5e7ba3b392cff14c8d5a9aba411210d6319
Atharv <atharvgupta098@gmail.com>
2026-04-02 23:54:54 +0530

    Learning context API

---
+++ 10todoContextAPI/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-500 text-center text-3xl'>Tailwind Installed</h1>
+    </>
+  )
+}
+
+export default App
