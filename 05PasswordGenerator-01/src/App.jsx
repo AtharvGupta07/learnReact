eed2b27d33f95596e9b0de5a82024ed1c5198e66
Atharv <atharvgupta098@gmail.com>
2026-09-03 05:07:58 +0530

    Optimised the paswordgenerator using useCallback and useRef

--- 05PasswordGenerator-01/src/App.jsx
+++ 05PasswordGenerator-01/src/App.jsx
@@ -1,19 +1,15 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 function App() {
-  const[val, setVal] = useState('8')
+  const[val, setVal] = useState(8);
   const[num, setNum] = useState(false);
   const[chr, setChr] = useState(false);  
   const[password, setPassword] = useState("");
 
-  const alphabet = "asdfghjklqwertyuiopzxcvbnm";
-  const numbers = "0123456789";
-  const specialCharacter = `!@#$%^&*(){}|:"<>?[]',./'`;
-
-  useEffect(()=>{
-    let passwordRange = alphabet;
-    if(num) passwordRange += numbers;
-    if(chr)  passwordRange += specialCharacter;
+  const passwordGenerator = useCallback(()=>{
+    let passwordRange = "ASDFGHJKLQWERTYUIOPZXCVBNMasdfghjklqwertyuiopzxcvbnm";
+    if(num) passwordRange += "0123456789";
+    if(chr)  passwordRange += "!@#$%^&*()";
 
     let newPassword = "";
 
@@ -23,18 +19,29 @@ function App() {
 
     setPassword(newPassword);
 
-  },[val,num,chr])
+  },[val, num, chr, setPassword])
 
-  const copyPassword = () => {
-    navigator.clipboard.writeText(password);
-  };
+  useEffect(()=>{
+    passwordGenerator();
+
+  },[val,num,chr,passwordGenerator])
+
+
+  const passwordRef = useRef(null);
+
+  const copyPassword = useCallback(()=>{
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  },[password])
+  
+  
 
   return (
-    <div className="bg-black w-full h-screen text-white flex justify-center items-start"> 
+    <div className="bg-slate-900 w-full h-screen flex justify-center items-start"> 
       <div className="bg-slate-800 m-20 p-5 rounded-xl text-orange-500">
         
         <div className="flex pb-5">
-          <input type="text" value={password} readOnly className="border-none outline-none flex-1 rounded-l-lg px-2" />
+          <input type="text" value={password} ref={passwordRef} readOnly className="border-none outline-none flex-1 rounded-l-lg px-2" />
           <button className="bg-blue-700 px-2 rounded-r-lg text-white hover:bg-blue-800" onClick={copyPassword}>Copy</button>
         </div>
         
@@ -46,12 +53,12 @@ function App() {
           </div>
 
           <div className="flex gap-1">
-            <input type="checkbox" checked={num} onClick={()=> setNum(!num)}/>
+            <input type="checkbox" checked={num} onChange = {()=> setNum((num) => !num)}/>
             <span>Number</span>
           </div>
 
           <div className="flex gap-1">
-            <input type="checkbox" checked={chr} onClick={()=> setChr(!chr)}/>
+            <input type="checkbox" checked={chr} onChange =  {()=> setChr((chr) =>!chr)}/>
             <span>Character</span>
           </div>
           
